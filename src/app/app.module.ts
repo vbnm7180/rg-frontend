168c96345bc5ee9ef10528e0c9a65fe1e5622b38
vbnm7180 <vbnm7180@mail.ru>
2021-08-08 18:46:54 +0400

    added footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HeaderModule } from './modules/header/header.module';
+import { FooterModule } from './modules/footer/footer.module';
 import { MainPageModule} from './modules/main-page/main-page.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './view/components/header/header.component';
+import { Footer } from 'primeng/api';
 //import { SilderComponent } from './silder/silder.component';
 //import { ConsolesComponent } from './consoles/consoles.component';
 
@@ -21,6 +23,7 @@ import { HeaderComponent } from './view/components/header/header.component';
     BrowserModule,
     AppRoutingModule,
     HeaderModule,
+    FooterModule,
     MainPageModule
   ],
   providers: [],
---
+++ src/app/modules/footer/footer.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import {MenuModule} from 'primeng/menu';
+import {MenubarModule} from 'primeng/menubar';
+
+import { FooterComponent } from 'src/app/view/components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MenuModule,
+    MenubarModule
+  ],
+  exports:[
+    FooterComponent
+  ]
+})
+export class FooterModule { }
