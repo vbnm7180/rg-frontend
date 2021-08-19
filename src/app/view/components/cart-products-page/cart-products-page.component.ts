3e813dd7b035c9999d52a0bb0fdf7e8bc98b4426
vbnm7180 <vbnm7180@mail.ru>
2021-08-19 17:12:50 +0400

    added text styles to cart product page

--- src/app/view/components/cart-products-page/cart-products-page.component.ts
+++ src/app/view/components/cart-products-page/cart-products-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/products';
 
 @Component({
   selector: 'rg-cart-products-page',
@@ -7,9 +8,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartProductsPageComponent implements OnInit {
 
+  public products: Product[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products = [
+			{
+				id: '1',
+				name: 'Sega Mega Drive (PAL) Серая в коробке',
+				price: 200000,
+				imagePath: '/test1',
+				categoryID: '1',
+				description: 'test desc',
+				attributes: [{
+					name: 'Регион',
+					value: 'PAL'
+				},
+				{
+					name: 'Состояние',
+					value: '4'
+				},
+				{
+					name: 'Комплект',
+					value: 'Полный'
+				}]
+			},
+			{
+				id: '2',
+				name: 'Sega Mega Drive 2',
+				price: 3000,
+				imagePath: '/test2',
+				categoryID: '1',
+				description: 'test desc',
+				attributes: [{
+					name: 'Регион',
+					value: 'PAL'
+				},
+				{
+					name: 'Состояние',
+					value: '5'
+				},
+				{
+					name: 'Комплект',
+					value: 'Полный'
+				}]
+			},
+			{
+				id: '1',
+				name: 'Sega Mega Drive 3',
+				price: 4000,
+				imagePath: '/test3',
+				categoryID: '1',
+				description: 'test desc',
+				attributes: [{
+					name: 'Регион',
+					value: 'PAL'
+				},
+				{
+					name: 'Состояние',
+					value: '3'
+				},
+				{
+					name: 'Комплект',
+					value: 'Неполный'
+				}]
+
+	        },
+      ];
   }
 
 }
