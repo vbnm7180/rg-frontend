d4c196ce4863a1417a7291f845c4e464876ef873
vbnm7180 <vbnm7180@mail.ru>
2022-06-03 18:15:08 +0400

    changed hide popup

--- src/app/services/popup-service/popup.service.ts
+++ src/app/services/popup-service/popup.service.ts
@@ -35,6 +35,11 @@ export class PopupService {
   }
 
   public hidePopup() {
-    this.popup = new Popup();
+    Object.assign(this.popup, {
+      popupHeader: '',
+      popupContent: '',
+      redirectUrl: '',
+      isPopupShown: false
+    });
   }
 }
